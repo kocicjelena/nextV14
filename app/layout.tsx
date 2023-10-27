aeba25a9601025a0ac19a3edfa0f69c00d174c6e
Jelena <kocicjelena@gmail.com>
2023-10-28 01:44:02 +0200

    meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import {inter} from '@/app/ui/fonts';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: {
+    template: '%s | follow',
+    default: 'Dashboard',
+  },
+  description: 'nextjs 14',
+  metadataBase: new URL('https://next-v14-kocicjelena.vercel.app'),
+};
+
+
 export default function RootLayout({
 
   children,
